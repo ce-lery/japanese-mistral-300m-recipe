8ddaae7eed940ff251f9d4969406afc116693ee8
ce-lery <celery3110@gmail.com>
2024-04-25 06:10:43 +0000

    style: jsonl入力時のassertionを除外

--- pretrain/train/run_clm.py
+++ pretrain/train/run_clm.py
@@ -236,10 +236,10 @@ class DataTrainingArguments:
         else:
             if self.train_file is not None:
                 extension = self.train_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`train_file` should be a csv, a json or a txt file."
+                # assert extension in ["csv", "json", "txt"], "`train_file` should be a csv, a json or a txt file."
             if self.validation_file is not None:
                 extension = self.validation_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`validation_file` should be a csv, a json or a txt file."
+                # assert extension in ["csv", "json", "txt"], "`validation_file` should be a csv, a json or a txt file."
 
 
 def main():
