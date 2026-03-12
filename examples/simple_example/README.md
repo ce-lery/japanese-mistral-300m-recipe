7aefd3522f95d24358006cccc4a77cd83909cd79
ce-lery <celery3110@gmail.com>
2026-03-12 16:39:20 +0000

    style: add simple pretraining example

---
+++ examples/simple_example/README.md
@@ -0,0 +1,15 @@
+# small example
+
+## Overview
+
+This is a simple pretraining example.
+
+## Getting Started
+
+```bash
+uv sync --extra build
+uv sync --extra build --extra compile
+bash dataset.sh
+bash train.sh
+```
+
---
+++ examples/simple_example/dataset.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Please run the command "cd pretrain/dataset/ ; bash dataset.sh"
+
+# ------------------------------------------
+#   Set log configuration
+# ------------------------------------------
+
+# set log
+mkdir -p ./results/log/$(basename "$0" .sh)
+log=./results/log/$(basename "$0" .sh)/$(date +%Y%m%d_%H%M%S).log
+exec &> >(tee -a $log)
+set -x
+
+# output start time
+
+echo ">>> start time: $(date '+%Y-%m-%d %I:%M:%S %p')"
+
+mkdir -p results/dataset/
+cd results/dataset/
+
+# ------------------------------------------
+#   Download tokenizer corpus
+# ------------------------------------------
+git lfs install
+GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/ce-lery/wiki.git
+cd wiki
+git lfs pull
+mv ./wiki.jsonl ../
+rm -r wiki
+
+echo ">>> end time: $(date '+%Y-%m-%d %I:%M:%S %p')"
